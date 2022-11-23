045a42b8f431bc979d993e1b6d3fda35d987cda8
Diego <Diego@DESKTOP-C6KVEL0>
2022-11-22 20:50:26 -0600

    Agregar archivos de proyecto.

---
+++ AppRadix_Sort/Form1.Designer.cs
@@ -0,0 +1,166 @@
+﻿namespace AppRadix_Sort
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtTamaño = new System.Windows.Forms.TextBox();
+            this.BtnIniciar = new System.Windows.Forms.Button();
+            this.LtbOrdenada = new System.Windows.Forms.ListBox();
+            this.LtbDesordenada = new System.Windows.Forms.ListBox();
+            this.BtnVaciar = new System.Windows.Forms.Button();
+            this.LblOrdenada = new System.Windows.Forms.Label();
+            this.LblDesOrdenada = new System.Windows.Forms.Label();
+            this.LblTamaño = new System.Windows.Forms.Label();
+            this.LtbTiempo = new System.Windows.Forms.ListBox();
+            this.LblTiempo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TxtTamaño
+            // 
+            this.TxtTamaño.Location = new System.Drawing.Point(12, 25);
+            this.TxtTamaño.Name = "TxtTamaño";
+            this.TxtTamaño.Size = new System.Drawing.Size(211, 20);
+            this.TxtTamaño.TabIndex = 0;
+            // 
+            // BtnIniciar
+            // 
+            this.BtnIniciar.Location = new System.Drawing.Point(377, 9);
+            this.BtnIniciar.Name = "BtnIniciar";
+            this.BtnIniciar.Size = new System.Drawing.Size(79, 47);
+            this.BtnIniciar.TabIndex = 1;
+            this.BtnIniciar.Text = "Iniciar";
+            this.BtnIniciar.UseVisualStyleBackColor = true;
+            this.BtnIniciar.Click += new System.EventHandler(this.BtnIniciar_Click);
+            // 
+            // LtbOrdenada
+            // 
+            this.LtbOrdenada.FormattingEnabled = true;
+            this.LtbOrdenada.Location = new System.Drawing.Point(298, 104);
+            this.LtbOrdenada.Name = "LtbOrdenada";
+            this.LtbOrdenada.Size = new System.Drawing.Size(210, 251);
+            this.LtbOrdenada.TabIndex = 2;
+            // 
+            // LtbDesordenada
+            // 
+            this.LtbDesordenada.FormattingEnabled = true;
+            this.LtbDesordenada.Location = new System.Drawing.Point(22, 104);
+            this.LtbDesordenada.Name = "LtbDesordenada";
+            this.LtbDesordenada.Size = new System.Drawing.Size(210, 251);
+            this.LtbDesordenada.TabIndex = 3;
+            // 
+            // BtnVaciar
+            // 
+            this.BtnVaciar.Location = new System.Drawing.Point(283, 9);
+            this.BtnVaciar.Name = "BtnVaciar";
+            this.BtnVaciar.Size = new System.Drawing.Size(79, 44);
+            this.BtnVaciar.TabIndex = 4;
+            this.BtnVaciar.Text = "Vaciar";
+            this.BtnVaciar.UseVisualStyleBackColor = true;
+            this.BtnVaciar.Click += new System.EventHandler(this.BtnVaciar_Click);
+            // 
+            // LblOrdenada
+            // 
+            this.LblOrdenada.AutoSize = true;
+            this.LblOrdenada.Location = new System.Drawing.Point(305, 88);
+            this.LblOrdenada.Name = "LblOrdenada";
+            this.LblOrdenada.Size = new System.Drawing.Size(57, 13);
+            this.LblOrdenada.TabIndex = 5;
+            this.LblOrdenada.Text = "Ordenada:";
+            // 
+            // LblDesOrdenada
+            // 
+            this.LblDesOrdenada.AutoSize = true;
+            this.LblDesOrdenada.Location = new System.Drawing.Point(19, 88);
+            this.LblDesOrdenada.Name = "LblDesOrdenada";
+            this.LblDesOrdenada.Size = new System.Drawing.Size(79, 13);
+            this.LblDesOrdenada.TabIndex = 6;
+            this.LblDesOrdenada.Text = "Des-Ordenada:";
+            // 
+            // LblTamaño
+            // 
+            this.LblTamaño.AutoSize = true;
+            this.LblTamaño.Location = new System.Drawing.Point(69, 9);
+            this.LblTamaño.Name = "LblTamaño";
+            this.LblTamaño.Size = new System.Drawing.Size(49, 13);
+            this.LblTamaño.TabIndex = 7;
+            this.LblTamaño.Text = "Tamaño:";
+            // 
+            // LtbTiempo
+            // 
+            this.LtbTiempo.FormattingEnabled = true;
+            this.LtbTiempo.Location = new System.Drawing.Point(462, 26);
+            this.LtbTiempo.Name = "LtbTiempo";
+            this.LtbTiempo.Size = new System.Drawing.Size(171, 30);
+            this.LtbTiempo.TabIndex = 8;
+            // 
+            // LblTiempo
+            // 
+            this.LblTiempo.AutoSize = true;
+            this.LblTiempo.Location = new System.Drawing.Point(462, 9);
+            this.LblTiempo.Name = "LblTiempo";
+            this.LblTiempo.Size = new System.Drawing.Size(45, 13);
+            this.LblTiempo.TabIndex = 9;
+            this.LblTiempo.Text = "Tiempo:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(636, 438);
+            this.Controls.Add(this.LblTiempo);
+            this.Controls.Add(this.LtbTiempo);
+            this.Controls.Add(this.LblTamaño);
+            this.Controls.Add(this.LblDesOrdenada);
+            this.Controls.Add(this.LblOrdenada);
+            this.Controls.Add(this.BtnVaciar);
+            this.Controls.Add(this.LtbDesordenada);
+            this.Controls.Add(this.LtbOrdenada);
+            this.Controls.Add(this.BtnIniciar);
+            this.Controls.Add(this.TxtTamaño);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtTamaño;
+        private System.Windows.Forms.Button BtnIniciar;
+        private System.Windows.Forms.ListBox LtbOrdenada;
+        private System.Windows.Forms.ListBox LtbDesordenada;
+        private System.Windows.Forms.Button BtnVaciar;
+        private System.Windows.Forms.Label LblOrdenada;
+        private System.Windows.Forms.Label LblDesOrdenada;
+        private System.Windows.Forms.Label LblTamaño;
+        private System.Windows.Forms.ListBox LtbTiempo;
+        private System.Windows.Forms.Label LblTiempo;
+    }
+}
+
---
+++ AppRadix_Sort/Form1.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AppRadix_Sort
+{
+    public partial class Form1 : Form
+    {
+        int[] arreglo;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void creararreglo()
+        {
+            int n= int.Parse(TxtTamaño.Text);
+            arreglo = new int[n];
+            Aleatorio();
+        }
+        private void Aleatorio()
+        {
+            Random R = new Random();
+            int Minimo = 0;
+            int Maximo = 100;
+            for (int I = 0; I < arreglo.Length; I++)
+            {
+                arreglo[I] = R.Next(Minimo, Maximo + 1);
+            }
+        }
+        private void Sort()
+        {
+            creararreglo();
+            int i, j;
+            int[] tmp = new int[arreglo.Length];
+            if (LtbDesordenada.Items.Count == 0)
+            {
+                for (int shift = 31; shift > -1; --shift)
+                {
+                    j = 0;
+                    for (i = 0; i < arreglo.Length; i++)
+                    {
+                        bool move = arreglo[i] << shift >= 0;
+                        if (shift == 0 ? move : move)
+                        {
+                            arreglo[i - j] = arreglo[i];
+                        }
+                        else
+                        {
+                            tmp[j++] = arreglo[i];
+                        }
+                    }
+                    Array.Copy(tmp, 0, arreglo, arreglo.Length - j, j);
+                }
+            }
+            else
+            {
+                for (int shift = 31; shift > -1; --shift)
+                {
+                    j = 0;
+                    for (i = 0; i < arreglo.Length; i++)
+                    {
+                        bool move = arreglo[i] << shift >= 0;
+                        if (shift == 0 ? move : move)
+                        {
+                            arreglo[i - j] = arreglo[i];
+                        }
+                        else
+                        {
+                            tmp[j++] = arreglo[i];
+                        }
+                    }
+                    Array.Copy(tmp, 0, arreglo, arreglo.Length - j, j);
+                }
+            }
+           
+        }
+        private void Mostrar()
+        {
+            creararreglo();
+            foreach (var item in arreglo)
+            {
+                LtbDesordenada.Items.Add(""+item);
+            }
+            Stopwatch tiempo = Stopwatch.StartNew();
+            tiempo.Start();
+            Sort();
+            tiempo.Stop(); 
+            foreach (var item in arreglo)
+            {
+                LtbOrdenada.Items.Add("" + item);
+            }
+            string mostrarT = "";
+            mostrarT = tiempo.Elapsed.ToString();
+            LtbTiempo.Items.Insert(0, mostrarT);
+        }
+        private void vaciar()
+        {
+            LtbDesordenada.Items.Clear();
+            LtbOrdenada.Items.Clear();
+            LtbTiempo.Items.Clear();
+            TxtTamaño.Clear();
+        }
+        private void BtnIniciar_Click(object sender, EventArgs e)
+        {
+            Mostrar();
+        }
+        private void BtnVaciar_Click(object sender, EventArgs e)
+        {
+            vaciar();
+        }
+    }
+}
